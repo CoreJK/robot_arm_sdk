b086a884115c6f4bf5ceaa6d3bb17b1d8dbd0322
coreJK <corejk@163.com>
2024-01-09 18:48:30 +0800

    🪡 Add: 添加正逆解接口

--- blinx_robot/blinx_robot/blinx_robot.py
+++ blinx_robot/blinx_robot/blinx_robot.py
@@ -5,6 +5,7 @@ import time
 from math import radians, degrees
 
 from loguru import logger
+import numpy as np
 from retrying import retry
 
 from blinx_robot_module import Mirobot
@@ -60,7 +61,7 @@ class BlxRobotArm(object):
         """
         command = json.dumps({"command": "set_joint_Auto_zero"}).replace(' ', "").strip() + '\r\n'
         self.client.send(command.encode('utf-8'))
-        data = self.client.recv(1024).decode()
+        data = json.loads(self.client.recv(1024).decode())
         return data
 
     def set_joint_angle(self, axle, angle):
@@ -70,7 +71,7 @@ class BlxRobotArm(object):
         """
         command = json.dumps({"command": "set_joint_angle", "data": [axle, angle]}).replace(' ', "").strip() + '\r\n'
         self.client.send(command.encode('utf-8'))
-        data = self.client.recv(1024).decode()
+        data = json.loads(self.client.recv(1024).decode())
         return data
 
     def set_joint_angle_speed(self, axle, angle, speed=50):
@@ -81,10 +82,9 @@ class BlxRobotArm(object):
         """
         command = json.dumps({"command": "set_joint_angle_speed", "data": [axle, angle, speed]}).replace(' ', "").strip() + '\r\n'
         self.client.send(command.encode('utf-8'))
-        data = self.client.recv(1024).decode()
+        data = json.loads(self.client.recv(1024).decode())
         return data
 
-    @limit_robot_arm_joint(6)
     def set_joint_angle_all_speed(self, *args, speed=50):
         """设置指定的机械臂关节角度
         :param args: 机械臂关节角度值
@@ -94,16 +94,35 @@ class BlxRobotArm(object):
         six_angle.append(speed)
         command = json.dumps({"command": "set_joint_angle_all_speed", "data": six_angle}).replace(' ', "").strip() + '\r\n'
         self.client.send(command.encode('utf-8'))
-        data = self.client.recv(1024).decode()
+        data = json.loads(self.client.recv(1024).decode())
         return data
 
-    @limit_robot_arm_joint(6)
-    def get_positive_solution(self, *args):
-        """获取机械臂正解"""
-        pass
+    def get_positive_solution(self, *args, current_pose=False):
+        """获取机械臂正解
+        :params args: 机械臂关节角度值
+        :params current_pose: 是否使用当前机械臂关节角度值
+        """
+        if current_pose:
+            joint_angle_list = self.get_joint_angle_all().get('data')
+        else:
+            joint_angle_list = list(*args)
+            
+        if joint_angle_list:
+            # 提取机械臂角度值
+            arm_pose_list = [round(float(data), 2) for data in joint_angle_list]
+            arm_pose_degrees = np.array(arm_pose_list)
+            # 计算机械臂正解
+            translation_vector = self.robot.fkine(arm_pose_degrees)
+            x, y, z = translation_vector.t  # 平移向量
+            Rz, Ry, Rx = map(lambda x: degrees(x), translation_vector.rpy())  # 旋转角
+            positive_solution = json.dumps({"command": "get_positive_solution", "data": [x, y, z, Rx, Ry, Rz]})
+            return positive_solution
+        else:
+            logger.error("获取机械臂角度值失败!")
+            positive_solution = json.dumps({"command": "get_positive_solution", "data": []})
+            return positive_solution
 
-    @limit_robot_arm_joint(6)
-    def get_inverse_solution(self, *args):
+    def get_inverse_solution(self):
         """获取机械臂逆解"""
         pass
 
@@ -114,29 +133,27 @@ class BlxRobotArm(object):
         """
         command = json.dumps({"command": "set_robot_io_interface", "data": [io, status]}).replace(' ', "").strip() + '\r\n'
         self.client.send(command.encode('utf-8'))
-        data = self.client.recv(1024).decode()
+        data = json.loads(self.client.recv(1024).decode())
         return data
-
-
 
     def get_joint_angle_all(self):
         """获取机械臂所有关节角度"""
         command = json.dumps({"command": "get_joint_angle_all"}).replace(' ', "").strip() + '\r\n'
         self.client.send(command.encode('utf-8'))
-        data = self.client.recv(1024).decode()
+        data = json.loads(self.client.recv(1024).decode())
         return data
-
+    
 
 if __name__ == "__main__":
     blx_robot_arm = BlxRobotArm("192.168.10.234", 1234)
-    print(blx_robot_arm.set_joint_auto_zero())
-    time.sleep(18)
+    # print(blx_robot_arm.set_joint_auto_zero())
+    # time.sleep(20)
     # print(blx_robot_arm.set_joint_angle(1, 45))
     # time.sleep(2)
     # print(blx_robot_arm.set_joint_angle_speed(1, 90, 50))
     # time.sleep(2)
-    # print(blx_robot_arm.set_joint_angle_all_speed(45, 90, 45, 90, 45, 90, 20, speed=50))
+    # print(blx_robot_arm.set_joint_angle_all_speed(50,50,50,50,50,50, speed=50))
     # time.sleep(2)
 
-    print(blx_robot_arm.get_joint_angle_all())
-
+    print(blx_robot_arm.get_positive_solution(45, 90, 45, 90, 45, 90, current_pose=True))
+    
